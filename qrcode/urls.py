8a2eb7a6d61a52b87684cfd0c5fa85ac9046acd6
vinicius-agross <vinicius.pontes@agross.com.br>
2025-05-02 08:35:15 -0300

    atualizacoes / exclusao do register.html

--- qrcode/urls.py
+++ qrcode/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from qrcode.views import index, aprovado, negado, validar, gerador, validador, cadastro, validado, gerar_pdf_qrcodes, checkin, buscar_email_por_cnpj, cadastrar_password, cadastrar_senha
+from qrcode.views import index, aprovado, negado, validar, gerador, validador, cadastro, validado, checkin, buscar_email_por_cnpj, cadastrar_password, cadastrar_senha
 
 urlpatterns = [
     path('', index, name="index"),
@@ -8,13 +8,11 @@ urlpatterns = [
     path("negado", negado, name="negado"),
     path("validado", validado, name="validado"),
     path("gerador/", gerador, name="gerador"),
-    path('gerar_pdf_qrcodes/', gerar_pdf_qrcodes, name='gerar_pdf_qrcodes'),
-    # path("gerador2/", gerador2, name="gerador2"),
     path('validador/', validador, name="validador"),
     path('cadastro/', cadastro, name="cadastro"),
     path('checkin/', checkin, name="checkin"),
     path('buscar-email/', buscar_email_por_cnpj,
          name='buscar_email_por_cnpj'),
     path('cadastrar-senha/', cadastrar_password, name='cadastrar_password'),
-    path('cadastrar-senha/confirmar/', cadastrar_senha, name='cadastrar_senha'),
+    path('cadastrar-senha/confirmar/', cadastrar_senha, name='cadastrar_senha')
 ]
--- setup/urls.py
+++ setup/urls.py
@@ -2,8 +2,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from allauth.account.views import SignupView
-from accounts.forms import CustomSignupForm
 
 
 urlpatterns = [
@@ -12,8 +10,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='cadastro/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', SignupView.as_view(
-        template_name='cadastro/register.html',
-        form_class=CustomSignupForm
-    ), name='register'),
 ]
