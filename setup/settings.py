0afccd6d30de5b3ebe928fc7b55c187039ff4264
vinicius-agross <vinicius.pontes@agross.com.br>
2025-05-02 10:49:09 -0300

    staticfilles

--- setup/settings.py
+++ setup/settings.py
@@ -74,9 +74,9 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfilles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'setup/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfilles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
