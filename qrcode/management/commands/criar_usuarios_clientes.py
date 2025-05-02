4beb88027c5b63bdf92a540304fad8fbe9b1c616
vinicius-agross <vinicius.pontes@agross.com.br>
2025-05-02 12:13:24 -0300

    bk

--- qrcode/management/commands/criar_usuarios_clientes.py
+++ qrcode/management/commands/criar_usuarios_clientes.py
@@ -1,35 +1,52 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from qrcode.models import ClienteQtd
+from django.db import transaction
 
 
 class Command(BaseCommand):
     help = 'Cria usuários Django a partir da tabela ClienteQtd com senha padrão baseada em CNPJ e email'
 
     def handle(self, *args, **kwargs):
-        clientes = ClienteQtd.objects.all()
-        for cliente in clientes:
-            if not cliente.email or not cliente.cnpj:
-                self.stdout.write(self.style.WARNING(
-                    f'Skipping: {cliente} (sem email ou CNPJ)'))
-                continue
+        # Buscar apenas os campos necessários
+        clientes = ClienteQtd.objects.only(
+            'cnpj', 'razsoc', 'email', 'qtd_convites')
 
-            username = cliente.cnpj
-            email = cliente.email
-            email_prefix = email.split('@')[0]
-            senha_padrao = f'{username[:4]}@{email_prefix}'
+        # Buscar usernames existentes em uma única consulta
+        usernames_existentes = set(
+            User.objects.values_list('username', flat=True))
 
-            if not User.objects.filter(username=username).exists():
-                user = User.objects.create_user(
+        novos_users = []
+
+        with transaction.atomic():  # Evita múltiplos commits desnecessários
+            for cliente in clientes:
+                if not cliente.email or not cliente.cnpj:
+                    self.stdout.write(self.style.WARNING(
+                        f'Skipping: {cliente} (sem email ou CNPJ)'))
+                    continue
+
+                username = cliente.cnpj
+
+                if username in usernames_existentes:
+                    self.stdout.write(f'Usuário já existe: {username}')
+                    continue
+
+                email = cliente.email
+                email_prefix = email.split('@')[0]
+                senha_padrao = f'{username[:4]}@{email_prefix}'
+
+                # Criação em memória (bulk create depois)
+                user = User(
                     username=username,
                     email=email,
-                    password=senha_padrao
+                    first_name=cliente.razsoc or ""
                 )
-                user.first_name = cliente.razsoc or ""
-                user.save()
+                user.set_password(senha_padrao)
+                novos_users.append(user)
 
-                self.stdout.write(self.style.SUCCESS(
-                    f'Usuário criado: {username} | senha: {senha_padrao}'
-                ))
-            else:
-                self.stdout.write(f'Usuário já existe: {username}')
+        # Inserir em lote (melhor performance)
+        if novos_users:
+            User.objects.bulk_create(novos_users)
+            self.stdout.write(self.style.SUCCESS(
+                f'{len(novos_users)} usuários criados com sucesso.'
+            ))
